4392493304267e3c665a934f542f787060956f8e
tttppp <tttppp@users.noreply.github.com>
2014-04-03 07:32:01 +0100

    Fix cells to fill screen.

--- src/main/java/com/github/tttppp/wordbox/WordBoxActivity.java
+++ src/main/java/com/github/tttppp/wordbox/WordBoxActivity.java
@@ -4,8 +4,9 @@ import java.util.Random;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TableLayout;
-import android.widget.TableRow;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class WordBoxActivity extends Activity {
@@ -15,18 +16,25 @@ public class WordBoxActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		TableLayout tableLayout = (TableLayout) findViewById(R.id.wordgrid);
+		LinearLayout wordGrid = (LinearLayout) findViewById(R.id.wordgrid);
 		for (int i = 0; i < 4; i++) {
-			TableRow row = new TableRow(this);
-			TableRow.LayoutParams lp = new TableRow.LayoutParams(
-			                                                     TableRow.LayoutParams.WRAP_CONTENT);
-			for (int j = 0; j < 4; j++) {
-				TextView box = new TextView(this);
-				box.setText(randomLetter());
-				row.addView(box);
-			}
+			LinearLayout row = new LinearLayout(this);
+			LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,
+			                                   LayoutParams.WRAP_CONTENT, 1f);
 			row.setLayoutParams(lp);
-			tableLayout.addView(row);
+			row.setGravity(Gravity.CENTER);
+			LayoutParams cellLayout = new LayoutParams(
+			                                           LayoutParams.WRAP_CONTENT,
+			                                           LayoutParams.MATCH_PARENT,
+			                                           1f);
+			for (int j = 0; j < 4; j++) {
+				TextView cell = new TextView(this);
+				cell.setLayoutParams(cellLayout);
+				cell.setGravity(Gravity.CENTER);
+				cell.setText(randomLetter());
+				row.addView(cell);
+			}
+			wordGrid.addView(row);
 		}
 	}
 
