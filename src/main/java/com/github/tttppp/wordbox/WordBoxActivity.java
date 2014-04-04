f1883919f2e5878a18c8de9961b17faa3c2f910b
tttppp <tttppp@users.noreply.github.com>
2014-04-04 21:53:38 +0100

    Letters are now sized about correct.

--- src/main/java/com/github/tttppp/wordbox/WordBoxActivity.java
+++ src/main/java/com/github/tttppp/wordbox/WordBoxActivity.java
@@ -1,13 +1,16 @@
 package com.github.tttppp.wordbox;
 
+import java.util.Arrays;
 import java.util.Random;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
+import android.widget.TextView.BufferType;
 
 import com.github.tttppp.wordbox.ui.component.FontFitTextView;
 
@@ -19,21 +22,26 @@ public class WordBoxActivity extends Activity {
 		setContentView(R.layout.main);
 
 		LinearLayout wordGrid = (LinearLayout) findViewById(R.id.wordgrid);
-		for (int i = 0; i < 4; i++) {
+		for (int i = 0; i < 7; i++) {
 			LinearLayout row = new LinearLayout(this);
 			LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT,
-			                                   LayoutParams.WRAP_CONTENT, 1f);
+			                                   LayoutParams.MATCH_PARENT, 1f);
+			lp.setMargins(0, 0, 0, 0);
 			row.setLayoutParams(lp);
-			row.setGravity(Gravity.CENTER);
+			row.setGravity(Gravity.TOP);
+			row.setPadding(1, 1, 1, 1);
 			LayoutParams cellLayout = new LayoutParams(
-			                                           LayoutParams.WRAP_CONTENT,
+			                                           LayoutParams.MATCH_PARENT,
 			                                           LayoutParams.MATCH_PARENT,
 			                                           1f);
+			cellLayout.setMargins(0, 0, 0, 0);
 			for (int j = 0; j < 4; j++) {
 				TextView cell = new FontFitTextView(this);
 				cell.setLayoutParams(cellLayout);
+				cell.setPadding(1, 1, 1, 1);
 				cell.setGravity(Gravity.CENTER);
-				cell.setText(randomLetter());
+				cell.setText(randomLetter(), BufferType.SPANNABLE);
+				cell.setIncludeFontPadding(false);
 				row.addView(cell);
 			}
 			wordGrid.addView(row);
@@ -44,5 +52,11 @@ public class WordBoxActivity extends Activity {
 		Random rand = new Random();
 		int i = rand.nextInt(26);
 		return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".substring(i, i + 1);
+	}
+
+	private int randomColor() {
+		Random rand = new Random();
+		int i = rand.nextInt(3);
+		return Arrays.asList(Color.RED, Color.CYAN, Color.YELLOW).get(i);
 	}
 }
--- src/main/java/com/github/tttppp/wordbox/ui/component/FontFitTextView.java
+++ src/main/java/com/github/tttppp/wordbox/ui/component/FontFitTextView.java
@@ -2,6 +2,7 @@ package com.github.tttppp.wordbox.ui.component;
 
 import android.content.Context;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.util.TypedValue;
 import android.widget.TextView;
@@ -10,6 +11,8 @@ import android.widget.TextView;
  * Auto resizing TextView (as found on StackOverflow).
  */
 public class FontFitTextView extends TextView {
+	/** Object used to test the bounds of the text. */
+	private Paint testPaint;
 
 	public FontFitTextView(Context context) {
 		super(context);
@@ -22,61 +25,66 @@ public class FontFitTextView extends TextView {
 	}
 
 	private void initialise() {
-		mTestPaint = new Paint();
-		mTestPaint.set(this.getPaint());
+		testPaint = new Paint();
+		testPaint.set(this.getPaint());
 		// max size defaults to the initially specified text size unless it is
 		// too small
 	}
 
 	/**
 	 * Re size the font so the specified text fits in the text box assuming the
-	 * text box is the specified width.
+	 * text box is the specified width and height.
 	 */
-	private void refitText(String text, int textWidth) {
+	private void refitText(String text, int textWidth, int textHeight) {
 		if (textWidth <= 0)
 			return;
 		int targetWidth = textWidth - this.getPaddingLeft()
 		    - this.getPaddingRight();
+		int targetHeight = textHeight - this.getPaddingTop()
+		    - this.getPaddingBottom();
 		float hi = 100;
 		float lo = 2;
 		final float threshold = 0.5f; // How close we have to be
 
-		mTestPaint.set(this.getPaint());
+		testPaint.set(this.getPaint());
 
 		while ((hi - lo) > threshold) {
 			float size = (hi + lo) / 2;
-			mTestPaint.setTextSize(size);
-			if (mTestPaint.measureText(text) >= targetWidth)
+			testPaint.setTextSize(size);
+			Rect bounds = new Rect();
+			testPaint.getTextBounds(text, 0, text.length(), bounds);
+			if (bounds.width() >= targetWidth
+			    || Math.abs(bounds.height()) >= Math.abs(targetHeight)) {
 				hi = size; // too big
-			else
+			} else {
 				lo = size; // too small
+			}
 		}
 		// Use lo so that we undershoot rather than overshoot
 		this.setTextSize(TypedValue.COMPLEX_UNIT_PX, lo);
+		this.scrollTo(0, Math.round(lo / 6));
 	}
 
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
 		int parentWidth = MeasureSpec.getSize(widthMeasureSpec);
+		int parentHeight = MeasureSpec.getSize(heightMeasureSpec);
 		int height = getMeasuredHeight();
-		refitText(this.getText().toString(), parentWidth);
+		refitText(this.getText().toString(), parentWidth, parentHeight);
 		this.setMeasuredDimension(parentWidth, height);
 	}
 
 	@Override
 	protected void onTextChanged(final CharSequence text, final int start,
 	                             final int before, final int after) {
-		refitText(text.toString(), this.getWidth());
+		refitText(text.toString(), this.getWidth(), this.getHeight());
 	}
 
 	@Override
 	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
 		if (w != oldw) {
-			refitText(this.getText().toString(), w);
+			refitText(this.getText().toString(), w, h);
 		}
 	}
-
-	// Attributes
-	private Paint mTestPaint;
 }
